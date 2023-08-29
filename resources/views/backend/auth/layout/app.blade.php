4abe160789b9d4ce4b10fc6428cb0aeab6de8c2b
mushahadur <mushahadurrahamankhanpulock@gmail.com>
2023-08-30 00:52:12 +0600

    register view

---
+++ resources/views/backend/auth/layout/app.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+
+    <head>
+        
+
+        @include('backend.auth.layout.partials.meta')
+
+
+        <title>Register | Minible - Admin & Dashboard Template</title>
+        
+        @include('backend.auth.layout.partials.style')
+
+
+    </head>
+
+    <body class="authentication-bg">
+
+        @yield('auth-contain')
+        
+
+        @include('backend.auth.layout.partials.script')
+        <!-- JAVASCRIPT -->
+
+
+    </body>
+</html>
